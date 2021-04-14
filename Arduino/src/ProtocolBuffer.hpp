7b0c1d95726462b437b2703de7c4ff682fc5d2c3
Martin Mlostek <mlostek@gmail.com>
2021-04-14 11:30:47 +0200

    added catch2 tests restructuring

---
+++ Arduino/src/ProtocolBuffer.hpp
@@ -0,0 +1,11 @@
+#ifndef __ProtocolBuffer_H__
+#define __ProtocolBuffer_H__
+
+
+class ProtocolBuffer
+{
+  
+};
+
+
+#endif /* __ProtocolBuffer_H__ */
---
+++ Arduino/test/ProtocolBufferTests.cpp
@@ -0,0 +1,12 @@
+#include "catch.hpp"
+#include "ProtocolBuffer.hpp"
+
+// This test writes the activations in csv file format, the data is then compared with external plotting tools.
+TEST_CASE("The test factory")
+{
+    REQUIRE(1 == 1);
+    REQUIRE(2 == 2);
+    REQUIRE(6 == 6);
+
+    ProtocolBuffer buf;
+}
